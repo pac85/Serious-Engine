3859766dfad309ab8b47122bd49263e852cc4847
Dreamy Cecil <stalker27production@gmail.com>
2023-03-01 19:29:01 +0300

    Make project files compatible with Visual Studio 2010.

--- Sources/SeriousSam/GUI/Menus/MConfirm.h
+++ Sources/SeriousSam/GUI/Menus/MConfirm.h
@@ -29,8 +29,13 @@ public:
   CMGButton gm_mgConfirmYes;
   CMGButton gm_mgConfirmNo;
 
-  void(*_pConfimedYes)(void) = NULL;
-  void(*_pConfimedNo)(void) = NULL;
+  void(*_pConfimedYes)(void);
+  void(*_pConfimedNo)(void);
+
+  CConfirmMenu() : CGameMenu(),
+    _pConfimedYes(NULL), _pConfimedNo(NULL)
+  {
+  };
 
   void Initialize_t(void);
   // return TRUE if handled
