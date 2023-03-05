a73abd5cfbfba351f68e7204d742f980c8ef746e
Dreamy Cecil <stalker27production@gmail.com>
2023-03-05 14:37:22 +0300

    Add CRC methods for getting unique integers out of pointers. - Fix CRC_AddWORD() mistakenly taking a UBYTE instead of a UWORD.

--- Sources/Engine/Base/CRC.h
+++ Sources/Engine/Base/CRC.h
@@ -30,7 +30,7 @@ inline void CRC_AddBYTE( ULONG &ulCRC, UBYTE ub)
   ulCRC = (ulCRC>>8)^crc_aulCRCTable[UBYTE(ulCRC)^ub];
 };
 
-inline void CRC_AddWORD( ULONG &ulCRC, UBYTE uw)
+inline void CRC_AddWORD( ULONG &ulCRC, UWORD uw)
 {
   CRC_AddBYTE(ulCRC, UBYTE(uw>> 8));
   CRC_AddBYTE(ulCRC, UBYTE(uw>> 0));
@@ -42,6 +42,18 @@ inline void CRC_AddLONG( ULONG &ulCRC, ULONG ul)
   CRC_AddBYTE(ulCRC, UBYTE(ul>>16));
   CRC_AddBYTE(ulCRC, UBYTE(ul>> 8));
   CRC_AddBYTE(ulCRC, UBYTE(ul>> 0));
+};
+
+inline void CRC_AddLONGLONG(ULONG &ulCRC, UQUAD ul)
+{
+  CRC_AddBYTE(ulCRC, UBYTE(ul >> 56));
+  CRC_AddBYTE(ulCRC, UBYTE(ul >> 48));
+  CRC_AddBYTE(ulCRC, UBYTE(ul >> 40));
+  CRC_AddBYTE(ulCRC, UBYTE(ul >> 32));
+  CRC_AddBYTE(ulCRC, UBYTE(ul >> 24));
+  CRC_AddBYTE(ulCRC, UBYTE(ul >> 16));
+  CRC_AddBYTE(ulCRC, UBYTE(ul >>  8));
+  CRC_AddBYTE(ulCRC, UBYTE(ul >>  0));
 };
 
 inline void CRC_AddFLOAT(ULONG &ulCRC, FLOAT f)
@@ -57,6 +69,38 @@ inline void CRC_AddBlock(ULONG &ulCRC, UBYTE *pubBlock, ULONG ulSize)
 
 // end crc calculation
 inline void CRC_Finish(ULONG &ulCRC) { ulCRC ^= 0xFFFFFFFF; };
+
+// In 32bit mode it just returns iPtr as ULONG,
+// In 64bit mode it returns the CRC hash of iPtr (or 0 if iPtr is NULL)
+// Either way you should get a value that very likely uniquely identifies the pointer
+inline ULONG IntPtrToID(size_t iPtr)
+{
+#if SE1_64BIT
+  // In case the code relies on 0 having special meaning because of NULL pointers
+  if (iPtr == 0) return 0;
+
+  ULONG ulRet;
+  CRC_Start(ulRet);
+  CRC_AddLONGLONG(ulRet, iPtr);
+  CRC_Finish(ulRet);
+  return ulRet;
+
+#else
+  return (ULONG)iPtr;
+#endif
+};
+
+// In 32bit mode it returns the pointer's address as ULONG
+// In 64bit mode it returns the CRC hash of the pointer's address (or 0 if ptr is NULL)
+// Either way you should get a value that very likely uniquely identifies the pointer
+inline ULONG PointerToID(void *ptr)
+{
+#if SE1_64BIT
+  return IntPtrToID((size_t)ptr);
+#else
+  return (ULONG)(size_t)ptr;
+#endif
+};
 
 #endif  /* include-once check. */
 
